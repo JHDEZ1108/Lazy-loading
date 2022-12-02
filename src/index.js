f9378545d861a30d3acd6b8f87eebea812b78e37
Josué Hernández <joheca1998@outlook.com>
2022-12-02 13:17:55 -0600

    Agregando imagenes mediante javascript

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//Creamos una imagen
+//Agregamos una imagen
+
+const createImageNode = () =>{
+  const container = document.createElement("div");
+  container.className= "p-5";
+  
+
+  const image = document.createElement("img");
+  image.className= "mx-auto";
+  image.src = "https://rickandmortyapi.com/api/character/avatar/2.jpeg";
+  
+  container.appendChild(image);
+  
+  return container;
+}
+
+const newImage = createImageNode();
+const mountNode = document.getElementById("images");
+
+mountNode.appendChild(newImage);
